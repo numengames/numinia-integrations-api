5128109ecd9b441cb2966798d940d840292d12de
Jesús Huerta Arrabal <jesushuertaarrabal@gmail.com>
2024-03-18 13:48:04 +0100

    feat: Include new params to the log message

--- src/route-versions/v1/controllers/discord.controller.ts
+++ src/route-versions/v1/controllers/discord.controller.ts
@@ -6,8 +6,8 @@ export default class DiscordController {
   private log = logger('DiscordController');
 
   sendWebhook(req: Request, res: Response) {
-    const { season, scapeRoomName, walletId } = req.body;
-    this.log.logInfo(`An user enter the scaperoom: ${scapeRoomName} as a user with walletId: ${walletId} (season ${season})`, { level: 'info', discord: true });
+    const { season, scapeRoomName, walletId, name } = req.body;
+    this.log.logInfo(`An user enter the scaperoom: ${scapeRoomName} as a user with walletId: ${walletId}, name: ${name} (season ${season})`, { level: 'info', discord: true });
 
     res.status(204).send();
   }
